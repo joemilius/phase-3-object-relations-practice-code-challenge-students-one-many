7bd520105899b65b15eac64ee5a5d22c97b11b12
JoeMilius <joemilius@gmail.com>
2021-07-28 18:58:52 -0600

    Completed Assignment

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,23 @@
 class Cohort < ActiveRecord::Base
-  
+  has_many :students, dependent: :destroy
+
+  def add_student(name, age)
+    Student.create(name: name, age: age, email: null, cohort_id: self.id)
+  end
+
+  def average_age
+    students.sum(:age).to_f / students.length
+  end
+
+  def total_students
+    students.length
+  end
+
+  def self.biggest
+    includes(:students).all.max_by {|cohort| cohort.students.length}
+  end
+
+  def self.sort_by_mod
+    all.sort_by {|cohort| cohort.current_mod}
+  end
 end
--- db/migrate/20191229140921_create_students.rb
+++ db/migrate/20191229140921_create_students.rb
@@ -3,6 +3,8 @@ class CreateStudents < ActiveRecord::Migration[5.2]
     create_table :students do |t|
       t.string :name
       t.integer :age
+      t.string :email
+      t.references :cohort, foreign_key: true, null: false
     end
   end
 end
